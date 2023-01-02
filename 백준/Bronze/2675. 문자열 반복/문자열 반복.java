7aa273c87358306c76ce5d882a2e7eec3e0b000d
NYC3644 <105707225+NYC3644@users.noreply.github.com>
2023-01-02 10:53:51 +0900

    [Bronze II] Title: 문자열 반복, Time: 232 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i<N; i++){
+            int R = sc.nextInt();
+            String str = sc.next();
+            for(int j = 0; j<str.length(); j++){
+                for(int k=0; k<R; k++) {
+                    System.out.print(str.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
